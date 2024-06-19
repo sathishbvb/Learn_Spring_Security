ea0d499d1024bbee2ecd5c5df327402de7b34c78
sathish <64584022+sathishbvb@users.noreply.github.com>
2024-06-19 22:12:31 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>This project is for learning spring security</h1>
+<div>
+    <label>Things to cover : </label>
+    <ol>
+        <li>Authentication.</li>
+        <li>Authorization.</li>
+        <li>Filter Chain.</li>
+    </ol>
+</div>
---
+++ src/main/java/com/sathish/security/security/LearnSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sathish.security.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringSecurityApplication.class, args);
+	}
+
+}
